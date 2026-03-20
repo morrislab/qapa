fa1246e61f4148fcafd60214663fbc9398ea7591
Kevin Ha <kcha@users.noreply.github.com>
2026-03-20 08:16:30 -0400

    Update environment

--- setup.py
+++ setup.py
@@ -8,38 +8,36 @@ if sys.version_info[:2] < (3, 8):
     sys.exit(1)
 
 here = os.path.abspath(os.path.dirname(__file__))
-exec(open(os.path.join(here, 'qapa/version.py')).read())
+exec(open(os.path.join(here, "qapa/version.py")).read())
 
-setup(name='qapa',
-      version=__version__,  # noqa: F821
-      description='RNA-seq Quantification of Alternative Polyadenylation (QAPA)',
-      url='http://github.com/morrislab/qapa',
-      author='Kevin Ha',
-      author_email='k.ha@mail.utoronto.ca',
-      license='GPLv3',
-      packages=find_packages(),
-      scripts=['scripts/create_merged_data.R',
-               'scripts/compute_pau.R'],
-      install_requires=['setuptools',
-                        'pandas >= 2.0.2',
-                        'numpy >= 1.10.0',
-                        'biopython >= 1.76',
-                        'pybedtools >= 0.7.9'
-                        ],
-      entry_points={
-          'console_scripts': [
-              'qapa = qapa.qapa:main'
-          ]
-      },
-      python_requires='~=3.8',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Topic :: Scientific/Engineering :: Bio-Informatics',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.8',
-      ],
-      zip_safe=False
-      )
+setup(
+    name="qapa",
+    version=__version__,  # noqa: F821
+    description="RNA-seq Quantification of Alternative Polyadenylation (QAPA)",
+    url="http://github.com/morrislab/qapa",
+    author="Kevin Ha",
+    author_email="k.ha@mail.utoronto.ca",
+    license="GPLv3",
+    packages=find_packages(),
+    scripts=["scripts/create_merged_data.R", "scripts/compute_pau.R"],
+    install_requires=[
+        "setuptools",
+        "pandas >= 2.0.2, <3.0",
+        "numpy >= 1.10.0, <2.0",
+        "biopython >= 1.76",
+        "pybedtools >= 0.7.9",
+    ],
+    entry_points={"console_scripts": ["qapa = qapa.qapa:main"]},
+    python_requires="~=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    zip_safe=False,
+)
